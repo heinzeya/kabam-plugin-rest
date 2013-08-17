398c3b8a71b3b4da67e5866103fdcfb164f69ba6
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-17 10:00:11 +0400

    [feature] - get one object from collection

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-exports.name='mwc_plugin_rest';
+exports.name = 'mwc_plugin_rest';
 
-exports.routes = function(mwc){
-  mwc.app.get(/^\/api\/rest\/([a-zA-Z0-9_]+)$/, function(request,response){
+exports.routes = function (mwc) {
+  mwc.app.get(/^\/api\/rest\/([a-zA-Z0-9_]+)$/, function (request, response) {
     var modelName = request.params[0];
 
-    if(request.model[modelName]){
-      if(request.user){
-        request.model[modelName].getForUser(request.user,{},function(err,documents){
-          if(err){
+    if (request.model[modelName]) {
+      if (request.user) {
+        request.model[modelName].getForUser(request.user, {}, function (err, documents) {
+          if (err) {
             throw err;
           } else {
             response.json(documents);
@@ -21,27 +21,29 @@ exports.routes = function(mwc){
     }
   });
 
-  mwc.app.post(/^\/api\/rest\/([a-zA-Z0-9_]+)\/?$/, function(request,response){
+  mwc.app.post(/^\/api\/rest\/([a-zA-Z0-9_]+)\/?$/, function (request, response) {
     var modelName = request.params[0];
-    if(request.model[modelName]){
-      if(request.user){
-        if(request.model[modelName].canCreate(request.user)){
+    if (request.model[modelName]) {
+      if (request.user) {
+        if (request.model[modelName].canCreate(request.user)) {
 
-          request.model[modelName].create(request.body,function(err,objCreated){
-            if(err) throw err;
-            if(request.is('json')){
-              if(objCreated){
+          request.model[modelName].create(request.body, function (err, objCreated) {
+            if (err) {
+              throw err;
+            }
+            if (request.is('json')) {
+              if (objCreated) {
                 response.status(202);
                 response.json(objCreated);
               } else {
                 response.send(400);
               }
             } else {
-              if(objCreated){
-                request.flash('success','Object created!');
+              if (objCreated) {
+                request.flash('success', 'Object created!');
                 response.redirect('back');
               } else {
-                request.flash('error','Unable to create object!');
+                request.flash('error', 'Unable to create object!');
                 response.redirect('back');
               }
             }
@@ -57,15 +59,40 @@ exports.routes = function(mwc){
     }
   });
 
-  mwc.app.get(/^\/api\/rest\/([a-zA-Z0-9_]+)\/([a-zA-Z0-9_]+)$/, function(request,response){
-    response.send('Show one item from collection of '+request.params[0] + ' with id of  '+request.params[1]);
+  mwc.app.get(/^\/api\/rest\/([a-zA-Z0-9_]+)\/([a-zA-Z0-9_]+)$/, function (request, response) {
+    response.send('Show one item from collection of ' + request.params[0] + ' with id of  ' + request.params[1]);
+    var modelName = request.params[0];
+    if (request.model[modelName]) {
+      if (request.user) {
+        request.model[modelName].findOne({'_id': request.params[1]}, function (err, objectFound) {
+          if (err) {
+            throw err;
+          }
+          if (objectFound) {
+            if (objectFound.canRead(request.user)) {
+              response.json(200,objectFound);
+            } else {
+              response.send(403);
+            }
+          } else {
+            response(404);
+          }
+        });
+      } else {
+        response.send(403);
+      }
+
+    }
+    else {
+      response.send(404);
+    }
   });
 
-  mwc.app.put(/^\/api\/rest\/([a-zA-Z0-9_]+)\/([a-zA-Z0-9_]+)$/, function(request,response){
-    response.send('Update one item from collection of '+request.params[0] + ' with id of  '+request.params[1]);
-  });
+    mwc.app.put(/^\/api\/rest\/([a-zA-Z0-9_]+)\/([a-zA-Z0-9_]+)$/, function (request, response) {
+      response.send('Update one item from collection of ' + request.params[0] + ' with id of  ' + request.params[1]);
+    });
 
-  mwc.app.delete(/^\/api\/rest\/([a-zA-Z0-9_]+)\/([a-zA-Z0-9_]+)$/, function(request,response){
-    response.send('Delete one item from collection of '+request.params[0] + ' with id of  '+request.params[1]);
-  });
-};
+    mwc.app.delete(/^\/api\/rest\/([a-zA-Z0-9_]+)\/([a-zA-Z0-9_]+)$/, function (request, response) {
+      response.send('Delete one item from collection of ' + request.params[0] + ' with id of  ' + request.params[1]);
+    });
+  };
